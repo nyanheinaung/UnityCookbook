3cbed7c07040f427a2297ebcabe46a4bc4379128
Jack Star <nyanheinaung@gmail.com>
2023-10-17 22:34:32 +0900

    SceneSwitch Complete

---
+++ HelloWorld/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void SwitchScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
