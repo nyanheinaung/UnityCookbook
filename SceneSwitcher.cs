755459b215b321bc4312ab73687df78d41fc054b
Jack Star <33681793+nyanheinaung@users.noreply.github.com>
2023-10-17 19:56:36 +0900

    Uploaded SceneSwitcher script

---
+++ SceneSwitcher.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void SwitchScene(string sceneName)
+	{
+		//find and read difference between LoadLevel and LoadScene(might need SceneManager)
+		Application.LoadLevel(sceneName);
+	}
+}
