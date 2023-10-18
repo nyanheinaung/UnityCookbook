fcc1c7167e6db26f94332ed3b070e64aaae4ee72
Jack Star <33681793+nyanheinaung@users.noreply.github.com>
2023-10-18 19:28:37 +0900

    Script for Panel Image and Slider UI

---
+++ HelloWorld/Assets/Scripts/Panel Image and Slider UI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void SwitchScene(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+}
+
+
+
+using UnityEngine.UI;
+
+public class ComeToFront : MonoBehaviour
+{
+	private Panel panelUI;
+
+	void Start()
+	{
+		panelUI = GetComponent<Panel>();
+	}
+
+	public void SwitchToFront()
+	{
+		int currentSiblingDepth = panelUI.GetSiblingDepth();
+		panelUI.SetSiblingDepth(currentSiblingDepth - 1);
+	}
+}
+
+
+public class SliderValueToText : MonoBehaviour
+{
+	public Text textSliderValue;
+	private Slider sliderUI;
+
+	void Start()
+	{
+		sliderUI = GetComponent<Slider>();
+		UpdateSliderText();
+	}
+
+	public void UpdateSliderText()
+	{
+		textSliderValue.Text = "Current slider value = " + sliderUI.Value;
+	}
+}
