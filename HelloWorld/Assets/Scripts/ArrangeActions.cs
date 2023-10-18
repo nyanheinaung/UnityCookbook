645afb5eba48f7937fd098e78a2fe26de165e1cb
Jack Star <nyanheinaung@gmail.com>
2023-10-18 21:53:38 +0900

    Panel Action 1st Commit

---
+++ HelloWorld/Assets/Scripts/ArrangeActions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArrangeActions : MonoBehaviour
+{
+	private RectTransform panelUI;
+
+	void Start()
+	{
+		panelUI = GetComponent<RectTransform>();
+	}
+
+	public void SwitchToFront()
+	{
+		int currentSiblingDepth = panelUI.GetSiblingIndex();
+		panelUI.SetSiblingIndex(currentSiblingDepth - 1);
+	}
+}
+
+
---
+++ HelloWorld/Assets/Scripts/SliderValueToText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderValueToText : MonoBehaviour
+{
+	public Text textSliderValue;
+	private Slider sliderUI;
+
+	void Start()
+	{
+		sliderUI = GetComponent<Slider>();
+		UpdateSliderText();
+	}
+
+	public void UpdateSliderText()
+	{
+		textSliderValue.text = "Current slider value = " + sliderUI.value;
+	}
+}
