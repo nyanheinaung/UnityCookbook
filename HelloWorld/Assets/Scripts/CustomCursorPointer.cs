4d5392855f7be4b1624f293b285c7307bd6b9295
Jack Star <nyanheinaung@gmail.com>
2023-10-23 22:48:27 +0900

    Custom Cursor Done

---
+++ HelloWorld/Assets/Scripts/CustomCursorPointer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomCursorPointer : MonoBehaviour
+{
+    public Texture2D cursorTexture2D;
+    public Texture2D defaultCursorTexture;
+
+    private CursorMode cursorMode = CursorMode.Auto;
+    private Vector2 hotspot = Vector2.zero;
+
+    private void Start()
+    {
+        OnMouseExit();    
+    }
+
+    public void OnMouseEnter()
+    {
+        SetCustomCursor(cursorTexture2D);
+    }
+
+    public void OnMouseExit()
+    {
+        SetCustomCursor(defaultCursorTexture);
+    }
+
+    private void SetCustomCursor(Texture2D curText)
+    {
+        Cursor.SetCursor(curText, hotspot, cursorMode);
+    }
+}
