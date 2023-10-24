e6877cce3cab798e078a9720e6995ac97de49b46
Jack Star <nyanheinaung@gmail.com>
2023-10-24 22:42:41 +0900

    Input Field Done

---
+++ HelloWorld/Assets/Scripts/DisplayChangeTextContent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayChangeTextContent : MonoBehaviour
+{
+    private InputField inputField;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputField = GetComponent<InputField>();
+    }
+
+    public void PrintNewValue()
+    {
+        string msg = "New content = ' " + inputField.text + "'";
+        print(msg);
+    }
+
+
+
+}
