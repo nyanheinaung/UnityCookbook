dc4e5891f5de639b6e1ee15100100b61f9aa3cec
Jack Star <nyanheinaung@gmail.com>
2023-10-25 23:50:11 +0900

    Toggle group testing commit

---
+++ HelloWorld/Assets/Scripts/RadioButtonManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RadioButtonManager : MonoBehaviour
+{
+    static Toggle currentDifficulty;
+    public string initialDifficulty = "Easy";
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!currentDifficulty)
+        {
+            currentDifficulty = GameObject.FindGameObjectWithTag(initialDifficulty).GetComponent<Toggle>();
+            currentDifficulty.isOn = true;
+        }
+        
+    }
+
+    public void PrintNewGroupValue(Toggle sender)
+    {
+        if (!sender.CompareTag(currentDifficulty.tag))
+        {
+            currentDifficulty.isOn = false;
+            print("Difficulty changed to : " + sender.tag);
+            currentDifficulty = sender;
+        }
+    }
+
+     // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ HelloWorld/Assets/Scripts/ToggleChangeManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleChangeManager : MonoBehaviour
+{
+    private Toggle toggle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        toggle = GetComponent<Toggle>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PrintNewValue()
+    {
+        bool status = toggle.isOn;
+        print("Toggle status : " + status);
+    }
+}
