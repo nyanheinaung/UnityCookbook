6899128bfac0f98f634d5f8213bac849db3e81f0
Jack Star <nyanheinaung@gmail.com>
2023-10-26 23:03:14 +0900

    Toggle Group Done

---
+++ HelloWorld/Assets/Scripts/ToggleGroupManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class ToggleGroupManager : MonoBehaviour
+{
+    ToggleGroup toggleGroup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        toggleGroup = GetComponent<ToggleGroup>();
+    }
+
+    public void Submit()
+    {
+        Toggle toggle = toggleGroup.ActiveToggles().FirstOrDefault();
+        print(toggle.GetComponentInChildren<Text>().text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
