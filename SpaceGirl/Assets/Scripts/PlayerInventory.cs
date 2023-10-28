ae9e6eb439b5f9a0261051b775c92f474829b4cb
Jack Star <nyanheinaung@gmail.com>
2023-10-28 23:39:25 +0900

    Displaying single object with text Done

---
+++ SpaceGirl/Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    private PlayerInventoryDisplay playerInventoryDisplay;
+    private bool carryingStar = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInventoryDisplay = GetComponent<PlayerInventoryDisplay>();
+        playerInventoryDisplay.OnChangeCarryingStar(carryingStar);
+    }
+
+    private void OnTriggerEnter2D(Collider2D hit)
+    {
+        if (hit.CompareTag("Star"))
+        {
+            carryingStar = true;
+            playerInventoryDisplay.OnChangeCarryingStar(carryingStar);
+            Destroy(hit.gameObject);
+        }
+    }
+
+}
---
+++ SpaceGirl/Assets/Scripts/PlayerInventoryDisplay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInventoryDisplay : MonoBehaviour
+{
+    public Text starText;
+
+    public void OnChangeCarryingStar(bool carryingStar)
+    {
+        string starMessage = "no star :-(";
+        if (carryingStar) starMessage = "Carrying star :-)";
+        starText.text = starMessage;
+    }
+
+}
