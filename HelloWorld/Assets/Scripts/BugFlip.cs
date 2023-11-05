beb54a68766817dfe251db779c58288b8c739f7b
Jack Star <nyanheinaung@gmail.com>
2023-11-05 22:18:38 +0900

    Bug Flip done

---
+++ HelloWorld/Assets/Scripts/BugFlip.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugFlip : MonoBehaviour
+{
+    private bool facingRight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow) && facingRight)
+        {
+            Flip();
+        }
+        
+        if(Input.GetKeyDown(KeyCode.RightArrow) && !facingRight)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
