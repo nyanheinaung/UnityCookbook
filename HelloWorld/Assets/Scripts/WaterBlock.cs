785e08ce31d4df32ab6811f2d27df93df2550537
Jack Star <nyanheinaung@gmail.com>
2023-11-07 22:50:49 +0900

    Falling Platform Done

---
+++ HelloWorld/Assets/Scripts/WaterBlock.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterBlock : MonoBehaviour
+{
+    private Animator animatorController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animatorController = GetComponent<Animator>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            animatorController.SetTrigger("Fall");
+            
+        }
+    }
+}
