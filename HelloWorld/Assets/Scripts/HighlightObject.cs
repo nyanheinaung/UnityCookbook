5f07fee6d89391a676c720d176e939c49d6dc04c
Jack Star <nyanheinaung@gmail.com>
2023-11-12 19:53:56 +0900

    Highlight At MouseOver Done

---
+++ HelloWorld/Assets/Scripts/HighlightObject.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightObject : MonoBehaviour
+{
+    private Color initialColor;
+    public bool noEmissionAtStart = true;
+    public Color highlightColor = Color.red;
+    public Color mousedownColor = Color.green;
+
+    private bool mouseOn = false;
+    private Renderer myRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRenderer = GetComponent<Renderer>();
+        if (noEmissionAtStart)
+        {
+            initialColor = Color.black;
+        }
+        else
+        {
+            initialColor = myRenderer.material.GetColor("_EmissionColor");
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        mouseOn = true;
+        myRenderer.material.SetColor("_EmissionColor", highlightColor);
+    }
+
+    private void OnMouseExit()
+    {
+        mouseOn = false;
+        myRenderer.material.SetColor("_EmissionColor", initialColor);
+    }
+
+    private void OnMouseDown()
+    {
+        myRenderer.material.SetColor("_EmissionColor", mousedownColor);
+    }
+
+    private void OnMouseUp()
+    {
+        if (mouseOn)
+        {
+            myRenderer.material.SetColor("_EmissionColor", highlightColor);
+        }
+        else
+        {
+            myRenderer.material.SetColor("_EmissionColor", initialColor);
+        }
+
+    }
+}
