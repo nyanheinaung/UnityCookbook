f16698e12ce9d2af341c5a09a3f0f31acf815031
Jack Star <nyanheinaung@gmail.com>
2023-11-21 23:33:45 +0900

    Video Texture in progress

---
+++ HelloWorld/Assets/Scripts/FadeMaterial.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeMaterial : MonoBehaviour
+{
+    public float fadeDuration = 1.0f;
+    public bool useMaterialAlpha = false;
+    public float alphaStart = 1.0f;
+    public float alphaEnd = 0.0f;
+    public bool destroyInvisibleObject = true;
+    private bool isFading = false;
+    private float alphaDiff;
+    private float startTime;
+    private Renderer rend;
+    private Color fadeColor;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        fadeColor = rend.material.color;
+
+        if (!useMaterialAlpha)
+        {
+            fadeColor.a = alphaStart;
+        }
+        else
+        {
+            alphaStart = fadeColor.a;
+        }
+        rend.material.color = fadeColor;
+        alphaDiff = alphaStart - alphaEnd;
+    }
+    void Update()
+    {
+        if (isFading)
+        {
+            var elapsedTime = Time.time - startTime;
+            if (elapsedTime <= fadeDuration)
+            {
+                var fadeProgress = elapsedTime / fadeDuration;
+                var alphaChange = fadeProgress * alphaDiff;
+                fadeColor.a = alphaStart - alphaChange;
+                rend.material.color = fadeColor;
+            }
+            else
+            {
+                fadeColor.a = alphaEnd;
+                rend.material.color = fadeColor;
+                if (destroyInvisibleObject)
+                    Destroy(gameObject);
+                
+                isFading = false;
+            }
+        }
+    }
+    void OnMouseUp()
+    {
+        FadeAlpha();
+    }
+    public void FadeAlpha()
+    {
+        isFading = true;
+        startTime = Time.time;
+    }
+}
+            
---
+++ HelloWorld/Assets/Scripts/PlayVideo.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class PlayVideo : MonoBehaviour
+{
+    public bool loop = true;
+    public bool playFromStart = true;
+    public MovieTexture video;
+    public AudioClip audioClip;
+    private AudioSource audio;
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        if (!video)
+            video = GetComponent<Renderer>().material.mainTexture as
+            MovieTexture;
+        if (!audioClip)
+            audioClip = audio.clip;
+        video.Stop();
+        audio.Stop();
+        video.loop = loop;
+        audio.loop = loop;
+        if (playFromStart)
+            ControlMovie();
+    }
+    void OnMouseUp()
+    {
+        ControlMovie();
+    }
+
+public void ControlMovie()
+    {
+        if (video.isPlaying)
+        {
+            video.Pause();
+            audio.Pause();
+        }
+        else
+        {
+            video.Play();
+            audio.Play();
+        }
+    }
+}
