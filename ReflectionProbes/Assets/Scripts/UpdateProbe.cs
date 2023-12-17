968245367add300bfcee29fd534e1f7fb6db2f25
Jack Star <nyanheinaung@gmail.com>
2023-12-17 23:12:11 +0900

    Reflection Probes Done

---
+++ ReflectionProbes/Assets/Scripts/UpdateProbe.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateProbe : MonoBehaviour
+{
+    private ReflectionProbe probe;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        probe = GetComponent<ReflectionProbe>();
+        probe.RenderProbe();
+    }
+
+    public void RefreshProbe()
+    {
+        probe.RenderProbe();
+    }
+}
