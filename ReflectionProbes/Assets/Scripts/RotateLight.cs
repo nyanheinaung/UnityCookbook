e7932ed74750980544af378526ec6d1640d110ea
Jack Star <nyanheinaung@gmail.com>
2023-12-18 23:23:01 +0900

    Procedural Skybox done

---
+++ ReflectionProbes/Assets/Scripts/RotateLight.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateLight : MonoBehaviour
+{
+    public float speed = -1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.right * speed * Time.deltaTime);        
+    }
+}
