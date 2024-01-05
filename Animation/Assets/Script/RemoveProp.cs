5f1989db04bb8096114c7e65c1b3943fc41f5ead
Jack Star <nyanheinaung@gmail.com>
2024-01-05 17:28:27 +0900

    Prop Remover Added

---
+++ Animation/Assets/Script/RemoveProp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveProp : MonoBehaviour
+{
+    public Transform targetBone;
+    public GameObject prop;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (targetBone.IsChildOf(collision.transform))
+        {
+            foreach(Transform child in targetBone)
+            {
+                if(child.name == prop.name)
+                {
+                    Destroy(child.gameObject);
+                }
+            }
+        }
+    }
+
+
+
+
+}
