4aeb5c5b11edfce2ed0dab8e8defdcc543f6d170
Jack Star <nyanheinaung@gmail.com>
2024-01-15 23:09:47 +0900

    Find Destination with NavMeshAgent

---
+++ NavMesh3D/Assets/Scripts/ArrowNPCMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ArrowNPCMovement : MonoBehaviour
+{
+    public GameObject targetGO;
+    private NavMeshAgent navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        HeadForDestination();
+    }
+
+    private void HeadForDestination()
+    {
+        Vector3 destination = targetGO.transform.position;
+        navMeshAgent.SetDestination(destination);
+    }
+}
