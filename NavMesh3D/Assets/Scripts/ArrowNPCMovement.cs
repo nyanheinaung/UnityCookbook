1d3aeab78d06e8dc917c78206eb8e56dc13edc0b
Jack Star <nyanheinaung@gmail.com>
2024-01-16 23:14:52 +0900

    Update flee away from player

--- NavMesh3D/Assets/Scripts/ArrowNPCMovement.cs
+++ NavMesh3D/Assets/Scripts/ArrowNPCMovement.cs
@@ -7,22 +7,35 @@ public class ArrowNPCMovement : MonoBehaviour
 {
     public GameObject targetGO;
     private NavMeshAgent navMeshAgent;
+    private float runAwayMultiplier = 2;
+    private float runAwayDistance;
 
     // Start is called before the first frame update
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
-        HeadForDestination();
+        runAwayDistance = navMeshAgent.stoppingDistance * runAwayMultiplier;
     }
 
     private void Update()
     {
-        HeadForDestination();    
+        Vector3 enemyPosition = targetGO.transform.position;
+        float distanceFromEnemy = Vector3.Distance(enemyPosition, transform.position);
+        if(distanceFromEnemy < runAwayDistance)
+        {
+            FleeFromTarget(enemyPosition);
+        }
     }
 
-    private void HeadForDestination()
+    private void FleeFromTarget(Vector3 enemyPosition)
     {
-        Vector3 destination = targetGO.transform.position;
+        Vector3 fleeToPosition = Vector3.Normalize(transform.position - enemyPosition) * runAwayDistance;
+        HeadForDestination(fleeToPosition);
+    }
+
+    private void HeadForDestination(Vector3 destination)
+    {
+        //Vector3 destination = targetGO.transform.position;
         navMeshAgent.SetDestination(destination);
     }
 }
