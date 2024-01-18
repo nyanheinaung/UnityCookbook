9c063c9c241a7509462f09d5c58eba8ce644e7eb
Jack Star <nyanheinaung@gmail.com>
2024-01-18 22:56:13 +0900

    Walk from One waypoint to another Done

---
+++ NavMesh3D/Assets/Scripts/WaypointManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointManager : MonoBehaviour
+{
+    public GameObject wayPoint0;
+    public GameObject wayPoint3;
+
+    public GameObject NextWayPoint(GameObject current)
+    {
+        if (current == wayPoint0)
+        {
+            return wayPoint3;
+        }
+        else
+        {
+            return wayPoint0;
+        }
+
+    }
+}
