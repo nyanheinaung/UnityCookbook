17b9423149a8c19673fa3a5c9ad259b43a4803c4
Jack Star <nyanheinaung@gmail.com>
2024-01-20 21:41:13 +0900

    Flocking Test Done

---
+++ Flocking/Assets/Scripts/Drone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Drone : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    public void SetTargetPosition(Vector3 swarmCenterAverage, Vector3 swarmMovementAverage)
+    {
+        Vector3 destination = swarmCenterAverage + swarmMovementAverage;
+        navMeshAgent.SetDestination(destination);
+    }
+
+}
