0d3f78141c8aa43c68bf5e4ab7ad0314817ae67e
Jack Star <nyanheinaung@gmail.com>
2024-02-06 23:33:39 +0900

    Play an Audio Clip (Improved)
    Preventing the clip from restarting if it is already playing

---
+++ ReverbZone/Assets/Scripts/AvoidEarlySoundRestart.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AvoidEarlySoundRestart : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public Text message;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        string statusMessage = "Play Sound";
+        if (audioSource.isPlaying)
+        {
+            statusMessage = "(Sound Playing)";
+        }
+        message.text = statusMessage;
+    }
+
+    // button click handler
+    public void PlaySoundEffectIfNotPlaying()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+    }
+}
