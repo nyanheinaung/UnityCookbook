937f62b82a193e0856cca3369d7682e9a14a4149
Jack Star <nyanheinaung@gmail.com>
2024-02-07 23:03:45 +0900

    Destroy after audio Playing Done

---
+++ ReverbZone/Assets/Scripts/ButtonActions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonActions : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioDestructBehaviour audioDestructScriptedObject;
+
+    public void PlaySound()
+    {
+        if (audioSource)
+        {
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+
+    }
+
+    public void DestroyAfterSoundStops()
+    {
+        if (audioSource)
+        {
+            audioDestructScriptedObject.enabled = true;
+        }
+
+    }
+}
