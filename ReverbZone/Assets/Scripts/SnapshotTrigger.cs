59c18f1e4e0e7d3f1e7dd0b349361d66a72f5364
Jack Star <nyanheinaung@gmail.com>
2024-02-13 23:15:44 +0900

    Snapshot Trigger Done

---
+++ ReverbZone/Assets/Scripts/SnapshotTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SnapshotTrigger : MonoBehaviour
+{
+    public AudioMixerSnapshot snapshot;
+    public float crossfade;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        snapshot.TransitionTo(crossfade);
+    }
+}
