ee4cb9d8f0e11fe0cbfc08ea299255bc392f51e8
Jack Star <nyanheinaung@gmail.com>
2024-02-17 23:57:37 +0900

    Load External Resources Basic Done

---
+++ SaveAndLoad/Assets/Scripts/ReadDefaultResources.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadDefaultResources : MonoBehaviour
+{
+    public string fileName = "externalTexture";
+    private Texture2D externalImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        externalImage = (Texture2D)Resources.Load(fileName);
+        Renderer myRenderer = GetComponent<Renderer>();
+        myRenderer.material.SetTexture("_MainTex", externalImage);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
