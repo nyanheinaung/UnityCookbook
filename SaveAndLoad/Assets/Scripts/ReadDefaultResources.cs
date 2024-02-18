99dcc7b3de1262895b0858299d6436addd3e441d
Jack Star <nyanheinaung@gmail.com>
2024-02-18 22:56:47 +0900

    Loading External Text Done

--- SaveAndLoad/Assets/Scripts/ReadDefaultResources.cs
+++ SaveAndLoad/Assets/Scripts/ReadDefaultResources.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof(AudioType))]
 public class ReadDefaultResources : MonoBehaviour
 {
     public string fileName = "externalTexture";
     private Texture2D externalImage;
+
+    public string textFileName = "textFileName";
+    private string textFileContents;
+
+    public string audioFileName = "soundtrack";
+    private AudioType audioFile;
 
     // Start is called before the first frame update
     void Start()
@@ -13,6 +20,14 @@ public class ReadDefaultResources : MonoBehaviour
         externalImage = (Texture2D)Resources.Load(fileName);
         Renderer myRenderer = GetComponent<Renderer>();
         myRenderer.material.SetTexture("_MainTex", externalImage);
+
+        TextAsset textAsset = (TextAsset)Resources.Load(textFileName);
+        textFileContents = textAsset.text;
+        Debug.Log(textFileContents);
+
+        AudioType audioSource = GetComponent<AudioType>();
+        //audioSource.clip
+
     }
 
     // Update is called once per frame
