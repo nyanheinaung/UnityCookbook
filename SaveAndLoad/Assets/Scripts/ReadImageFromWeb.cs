964546ce0672727dd481285d48209903f8fbb488
Jack Star <nyanheinaung@gmail.com>
2024-02-21 23:15:22 +0900

    Get data from website using WebRequest Done

--- SaveAndLoad/Assets/Scripts/ReadImageFromWeb.cs
+++ SaveAndLoad/Assets/Scripts/ReadImageFromWeb.cs
@@ -2,27 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
 public class ReadImageFromWeb : MonoBehaviour
 {
-    public string url = "www.ascii-art.de/ascii/ab/badger.txt";
+    public string url = "http://www.ascii-art.de/ascii/ab/badger.txt";
 
-    IEnumerator Start()
+/*    void Start()
     {
-        /*WWW www = new WWW(url);
-        yield return www;
-        Texture2D texture = www.texture;
-        GetComponent<RawImage>().texture = texture;*/
+        StartCoroutine(MakeWebRequest());
+    }*/
+
+ /*   IEnumerator MakeWebRequest()
+    {
 
         Text textUI = GetComponent<Text>();
         textUI.text = "(Loading file...)";
-        WWW www = new WWW(url);
-        yield return www;
-        string textFileContents = www.text;
+        using (UnityWebRequest www = UnityWebRequest.Get(url))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+                Debug.LogError($"Error : {www.error}");
+            }
+            else
+            {
+                string textFileContents = www.downloadHandler.text;
+                Debug.Log(textFileContents);
+                textUI.text = textFileContents;
+            }
+
+        } 
+    }*/
+
+    IEnumerator Start()
+    {
+        Text textUI = GetComponent<Text>();
+        textUI.text = "(Loading file...)";
+        UnityWebRequest www = UnityWebRequest.Get(url);
+        yield return www.SendWebRequest();
+        string textFileContents = www.downloadHandler.text;
         Debug.Log(textFileContents);
-
         textUI.text = textFileContents;
-
     }
-
 }
