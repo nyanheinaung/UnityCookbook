89973cfaca2fd8df405d59afc838ae48fe9894c3
Jack Star <nyanheinaung@gmail.com>
2024-02-24 13:46:18 +0900

    Saving and loading player data with static properties done

---
+++ game_higherOrLower/Assets/HigherOrLower/Scripts/IncrementCorrectScore.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncrementCorrectScore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player.scoreCorrect++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ game_higherOrLower/Assets/HigherOrLower/Scripts/UpdateScoreText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateScoreText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text scoreText = GetComponent<Text>();
+        int totalAttempts = Player.scoreCorrect + Player.scoreIncorrect;
+        string scoreMessage = "";
+        if (totalAttempts > 0)
+        {
+            scoreMessage = "Score:";
+            scoreMessage += Player.scoreCorrect + "/" + totalAttempts;
+        }
+        
+        scoreText.text = scoreMessage;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
